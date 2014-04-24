57a1163de683805fb1585ec1bcb77b8fbe5331df
yuanyu.chang <yuanyu.chang@coretronic.com>
2014-04-24 15:16:50 +0800

    Initial Commit

---
+++ postData_sample_AFNetworking/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  postData_sample_AFNetworking
+//
+//  Created by Chang YuanYu on 2014/4/24.
+//  Copyright (c) 2014年 James. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ postData_sample_AFNetworking/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  postData_sample_AFNetworking
+//
+//  Created by Chang YuanYu on 2014/4/24.
+//  Copyright (c) 2014年 James. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
